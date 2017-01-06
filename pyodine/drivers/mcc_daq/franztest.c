047073a6cf9b60d1e1a776be6f5dfd15d6714d28
Franz Gutsch <franz.balthasar-git@gmail.com>
2017-01-06 23:16:36 +0100

    Plastic surgery.

--- pyodine/drivers/mcc_daq/franztest.c
+++ pyodine/drivers/mcc_daq/franztest.c
@@ -2,7 +2,8 @@
 
 int main() {
   libusb_device_handle *dev = OpenConnection();
-  float out_cal[NCHAN_AO_1608GX][2], in_cal[NGAINS_1608G][2];
+  float out_cal[NCHAN_AO_1608GX][2];
+  float in_cal[NGAINS_1608G][2];
   GenerateCalibrationTable(dev, in_cal, out_cal); 
   Sawtooth(dev, out_cal);
 }
