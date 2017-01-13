fe44059b4b5290f8c4007f027c9b0d8bd28e7b97
Franz Gutsch <franz.balthasar-git@gmail.com>
2017-01-13 15:01:00 +0100

    "Progress towards" Menlo stack driver...

--- pyodine/drivers/menlo_stack.py
+++ pyodine/drivers/menlo_stack.py
@@ -1,45 +1,63 @@
 """An interface wrapper to the Menlo stack websocket.
-
 This module provides an interface wrapper class for the websockets interface
 exposed by the Menlo Electronics control computer.
 """
 
 import logging     # DEBUG, INFO, WARN, ERROR etc.
+import time        # To keep track of when replies came in.
 import asyncio     # A native python module needed for websockets.
 import websockets
-
 logger = logging.getLogger('pyodine.drivers.menlo_stack')
+
+# Constants specific to the published Menlo interface.
+
+LASER_NODES = [3, 4, 5, 6]  # node IDs of laser units 1 through 4
+LOCKBOX_NODES = [1, 2]      # node IDs of lockboxes 1 and 2
+ADC_NODE = 16               # node ID of the analog-digital converter
+MUC_NODE = 255              # node ID of the embedded system
+LASER_SERVICES = {'set_temperature': 1,
+                  'set_ld_current': 2,
+                  'enable_diode': 5,
+                  'diode_temp': 272,
+                  'tec_current': 274,
+                  'diode_current': 275,
+                  'temp_ok': '288'}
+LOCKBOX_SERVICES = {'disable': 0,
+                    '?1': 1,
+                    '?2': 2,
+                    'ramp_enable': 3,
+                    '?4': 4,
+                    '?5': 5,
+                    '?6': 6,
+                    '?272': 272,
+                    '?273': 273}
+MUC_SERVICES = {'time': 1}
+
+ROTATE_N = 10  # Keep log of received values smaller than this.
 
 
 class MenloStack:
     """Provides an interface to the Menlo electronics stack."""
 
-    LASER_NODES = {1: '3', 2: '4', 3: '5', 4: '6'}
-    LOCKBOX_NODES = {1: '1', 2: '2'}
-    ADC_NODE = 16
-    LASER_SERVICES = {'set_temperature': '1',
-                      'set_ld_current': '2',
-                      'enable_diode': '5',
-                      'diode_temp': '272',
-                      'tec_current': '274',
-                      'diode_current': '275',
-                      'temp_ok': '288'}
-    LOCKBOX_SERVICES = {'disable': '0',
-                        '?1': '1',
-                        '?2': '2',
-                        'ramp_enable': '3',
-                        '?4': '4',
-                        '?5': '5',
-                        '?6': '6',
-                        '?272': '272',
-                        '?273': '273'}
+    ##############
+    # Properties #
+    ##############
+
+    @property
+    def muc_times(self) -> list:
+        return self._muc_times
+
+    ####################
+    # Instance Methods #
+    ####################
+
+    # Laser Diode Driver Control.
 
     def __init__(self, url: str='ws://menlostack:8000'):
         """Establish a websocket connection with the Menlo stack controller.
         """
-        self.conn = websockets.connect(url)
-
-    # Laser Diode Driver Control.
+        self.client = websockets.connect(url)
+        self._muc_times = []  # List of times received from MUC
 
     def laser_enable(self, enable: bool=True, unit: int=1) -> None:
         pass
@@ -77,6 +95,51 @@ class MenloStack:
 
     def _send_string(self, message: str) -> None:
         pass
+
+    def _parse_reply(self, received_string: str) -> tuple:
+        logger.debug("Parsing reply " + received_string)
+        parts = received_string.split(":")
+        return parts[0], parts[1], parts[2]  # node, service, value
+
+    def _store_reply(self, node: int, service: int, value: str) -> None:
+        if node in LASER_NODES:
+            unit = LASER_NODES.index(node)
+            self._store_laser_param(unit, service, value)
+
+        elif node in LOCKBOX_NODES:
+            unit = LOCKBOX_NODES.index(node)
+            self._store_lockbox_param(unit, service, value)
+
+        elif node == ADC_NODE:
+            self._store_adc_param(service, value)
+
+        elif node == MUC_NODE:
+            self._store_muc_param(service, value)
+
+        else:
+            logger.warning("Unknown node ID.")
+
+    def _store_laser_param(self, unit: int, service: int, value: str) -> None:
+        pass
+
+    def _store_muc_param(self, service, value) -> None:
+        if service in MUC_SERVICES.values():
+            if service == MUC_SERVICES["time"]:
+                self.add_to_rotating_log(self._muc_times, (value, time.time()))
+        else:
+            logger.warning("Unknown MUC service ID {}".format(service))
+
+    ##################
+    # Static Methods #
+    ##################
+
+    @staticmethod
+    def add_to_rotating_log(log_list: list, value) -> None:
+        log_list.insert(0, value)
+
+        # Shave of some elements if list gets too long.
+        if len(log_list) > ROTATE_N:
+            log_list = log_list[0:ROTATE_N-1]
 
 
 if __name__ == '__main__':
