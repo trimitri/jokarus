5a0c0a89aa6873994c13fbfb92a7a32fa34e2ebf
Franz Gutsch <franz.balthasar-git@gmail.com>
2017-01-18 15:16:10 +0100

    Created buffers for all incoming data.

--- pyodine/drivers/menlo_stack.py
+++ pyodine/drivers/menlo_stack.py
@@ -7,7 +7,9 @@ import logging     # DEBUG, INFO, WARN, ERROR etc.
 import time        # To keep track of when replies came in.
 import asyncio     # A native python module needed for websockets.
 import websockets
-logger = logging.getLogger('pyodine.drivers.menlo_stack')
+
+LOGGER = logging.getLogger('pyodine.drivers.menlo_stack')
+ROTATE_N = 10  # Keep log of received values smaller than this.
 
 # Constants specific to the published Menlo interface.
 
@@ -15,41 +17,31 @@ LASER_NODES = [3, 4, 5, 6]  # node IDs of laser units 1 through 4
 LOCKBOX_NODES = [1, 2]      # node IDs of lockboxes 1 and 2
 ADC_NODE = 16               # node ID of the analog-digital converter
 MUC_NODE = 255              # node ID of the embedded system
-LASER_SERVICES = {'set_temperature': 1,
-                  'set_ld_current': 2,
-                  'enable_diode': 5,
-                  'diode_temp': 272,
-                  'tec_current': 274,
-                  'diode_current': 275,
-                  'temp_ok': '288'}
-LOCKBOX_SERVICES = {'disable': 0,
-                    '?1': 1,
-                    '?2': 2,
-                    'ramp_enable': 3,
-                    '?4': 4,
-                    '?5': 5,
-                    '?6': 6,
-                    '?272': 272,
-                    '?273': 273}
-MUC_SERVICES = {'time': 1}
 
-ROTATE_N = 10  # Keep log of received values smaller than this.
+# Provide dictionaries for the service IDs.
+LASER_SVC_GET = {272: 'diode_temp',
+                 274: 'tec_current',
+                 275: 'diode_current',
+                 288: 'temp_ok'}
+LASER_SVC_SET = {1: 'diode_temp',
+                 2: 'diode_current',
+                 3: 'enable_diode'}
+LOCKBOX_SVC_GET = {272: "not well documented (Lockbox Monitor)",  # FIXME
+                   273: "not well documented (P Monitor)"}  # FIXME
+LOCKBOX_SVC_SET = {0: 'disable',
+                   1: '?1',
+                   2: '?2',
+                   3: 'ramp_enable',
+                   4: '?4',
+                   5: '?5',
+                   6: '?6'}
+ADC_SVC_GET = {}
+ADC_SVC_SET = {}
+MUC_SVC_GET = {1: 'time'}
 
 
 class MenloStack:
     """Provides an interface to the Menlo electronics stack."""
-
-    ##############
-    # Properties #
-    ##############
-
-    @property
-    def muc_times(self) -> list:
-        return self._muc_times
-
-    ####################
-    # Instance Methods #
-    ####################
 
     # Laser Diode Driver Control.
 
@@ -58,16 +50,25 @@ class MenloStack:
         """
         self._ws = websockets.connect(url)
 
-        # Create buffers for saving received system parameters.
-
-        # Laser drivers
-        self._diode_temps = []     # laser diode temperature
-        self._tec_currents = []    # Peltier currents
-        self._diode_currents = []  # laser diode current
-        self._laser_drv_ok = []    # "OK" flag of the laser driver
-
-        # Embedded System
-        self._muc_times = []       # MUC system time
+        # Create one buffer for each receiving service of each laser driver
+        # using a nested double dict comprehension.
+        # Then do the same for the lockboxes.
+        # The finished dicts will look as follows:
+        #   {
+        #      node_id1: {svc_id1: [], svc_id2: [], etc.},
+        #      node_id2: {svc_id1: [], svc_id2: [], etc.},
+        #      etc.
+        #   }
+        # Each of the contained empty lists will eventually hold received data
+        # in tuples: [(value1, time1), (value2, time2), etc.]
+        self._laser_buffers = {
+            node_id: {svc_id: [] for svc_id in LASER_SVC_GET}
+            for node_id in LASER_NODES}
+        self._lockbox_buffers = {
+            node_id: {svc_id: [] for svc_id in LOCKBOX_SVC_GET}
+            for node_id in LOCKBOX_NODES}
+        self._adc_buffers = {svc_id: [] for svc_id in ADC_SVC_GET}
+        self._muc_buffers = {svc_id: [] for svc_id in MUC_SVC_GET}
 
         # Schedule background tasks to run in central asyncio event loop.
 
@@ -101,8 +102,6 @@ class MenloStack:
     def lockbox_enable(self, enable: bool=True, unit: int=1) -> None:
         pass
 
-    # Private Methods
-
     def _send_command(self, node: int, service: int, value: str) -> None:
         message = str(node) + ':0:' + str(service) + ':' + str(value)
         self._send_string(message)
@@ -110,8 +109,9 @@ class MenloStack:
     def _send_string(self, message: str) -> None:
         pass
 
-    def _parse_reply(self, received_string: str) -> tuple:
-        logger.debug("Parsing reply " + received_string)
+    @staticmethod
+    def _parse_reply(received_string: str) -> tuple:
+        LOGGER.debug("Parsing reply '%s'", received_string)
         parts = received_string.split(":")
         return(int(parts[0]), int(parts[1]), parts[2])  # node, service, value
 
@@ -131,26 +131,35 @@ class MenloStack:
             self._store_muc_param(service, value)
 
         else:
-            logger.warning("Unknown node ID.")
+            LOGGER.warning("Unknown node ID.")
 
     def _store_laser_param(self, unit: int, service: int, value: str) -> None:
-        pass
+        LOGGER.debug("Storing Laser %s param %s as %s.", unit, service, value)
+        if service in LASER_SVC_GET:
+            pass  # TODO
+        else:
+            LOGGER.error("Unknown laser controller service id (%s).", service)
+
+    def _store_lockbox_param(self,
+                             unit: int, service: int, value: str) -> None:
+        LOGGER.debug("Storing Lockbox %s param %s as %s.", unit, service,
+                     value)
+        # TODO
+
+    def _store_adc_param(self, service: int, value: str) -> None:
+        LOGGER.debug("Storing ADC param %s as %s.", service, value)
+        # TODO
 
     def _store_muc_param(self, service, value) -> None:
-        if service in MUC_SERVICES.values():
-            if service == MUC_SERVICES["time"]:
-                self.add_to_rotating_log(self._muc_times, (value, time.time()))
-        else:
-            logger.warning("Unknown MUC service ID {}".format(service))
+        LOGGER.debug("Storing MUC param %s as %s.", service, value)
+        # TODO
 
     async def _listen_to_socket(self) -> None:
         while True:
             # message = await self._ws.recv()
             message = await self._mock_reply()
             self._store_reply(*self._parse_reply(message))
-            await asyncio.sleep(2.3)
 
-    # TODO remove this.
     @staticmethod
     async def _mock_reply() -> str:
         await asyncio.sleep(.2)  # Simulate websocket wait.
@@ -179,8 +188,8 @@ class MenloStack:
 if __name__ == '__main__':
 
     async def hello_menlo() -> None:
-        async with websockets.connect('ws://menlostack:8000') as ws:
+        async with websockets.connect('ws://menlostack:8000') as sckt:
             while True:
-                print(await ws.recv())
+                print(await sckt.recv())
 
     asyncio.get_event_loop().run_until_complete(hello_menlo())
