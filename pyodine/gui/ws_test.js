e9c6c1d60068238a2273564b9385175c874811ac
Franz Gutsch <franz.balthasar-git@gmail.com>
2017-03-14 10:44:31 +0100

    Tabbed layout for UI.

--- pyodine/gui/ws_test.js
+++ pyodine/gui/ws_test.js
@@ -1,10 +1,8 @@
 'use strict';
 
-function log(text) {
-  document.body.appendChild(document.createTextNode(text + '\n'));
-}
+jQuery(function(){
 
-var ws = new WebSocket("ws://localhost:12345/");
-ws.onmessage = function (event) {
-  log(event.data);
-};
+  $('#tabs').tabs();
+
+  var ws = new WebSocket("ws://localhost:12345/");
+});
