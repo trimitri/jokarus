2f55978d7ad207d91ae2a9ce8a818365b759eaa3
Franz Gutsch <franz.balthasar-git@gmail.com>
2017-03-14 18:09:29 +0100

    Starting to get JS structured.

--- pyodine/gui/pyodine-gui.js
+++ pyodine/gui/pyodine-gui.js
@@ -2,35 +2,13 @@
 
 jQuery(function(){
 
-  async function foo() {
-    // Setup layout using jQuery UI.
-    $('#tabs').tabs();
-
-    var messageHandler = function(event) {
-      document.body.appendChild(document.createTextNode(event.data)); 
-    };
-
-    var ws;
-    $('#connect_btn').on('click', function() {
-      var host = $('#host').val();
-      var ws_port = $('#ws_port').val();
-      ws = new WebSocket('ws://' + host + ':' + ws_port + '/');
-      ws.onmessage = messageHandler;
-    });
-
+  async function createDummyStreamer(plotElm, title) {
     var temp = {
       y: [0],
       mode: 'lines'
     };
     var data = [temp];
-    var layout = {
-      title: 'SUP1 Laser Diode Temp.'
-    };
-
-    function sleep(ms) {
-      return new Promise(resolve => setTimeout(resolve, ms));
-    }
-
+    var layout = {title: title};
     var plt = document.getElementById('sup1_ld_temp');
     var display_range = 100;
 
@@ -42,5 +20,26 @@ jQuery(function(){
     }
   }
 
-  foo();
+  // Setup layout using jQuery UI.
+  $('#tabs').tabs();
+
+  var messageHandler = function(event) {
+    document.body.appendChild(document.createTextNode(event.data)); 
+  };
+
+  function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+  createDummyStreamer(document.getElementById('sup1_ld_temp'), "SUP1 Laser Diode Temp.");
+  // createDummyStreamer(document.getElementById('sup1_ld_current'), "SUP1 Laser Diode Current");
+  // createDummyStreamer(document.getElementById('sup1_tec_current'), "SUP1 Peltier Current");
+
+  var ws;
+  $('#connect_btn').on('click', function() {
+    var host = $('#host').val();
+    var ws_port = $('#ws_port').val();
+    ws = new WebSocket('ws://' + host + ':' + ws_port + '/');
+    ws.onmessage = messageHandler;
+  });
 });
