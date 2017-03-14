762f15232dfb7bfd1d282678e0e1ead918c68524
Franz Gutsch <franz.balthasar-git@gmail.com>
2017-03-14 13:42:10 +0100

    GUI: Added a message handler.

--- pyodine/gui/pyodine-gui.js
+++ pyodine/gui/pyodine-gui.js
@@ -5,11 +5,16 @@ jQuery(function(){
   // Setup layout using jQuery UI.
   $('#tabs').tabs();
 
+  var messageHandler = function(event) {
+   document.body.appendChild(document.createTextNode(event.data)); 
+  };
+
   // Connect to server.
   var ws;
   $('#connect_btn').on('click', function() {
     var host = $('#host').val();
     var ws_port = $('#ws_port').val();
     ws = new WebSocket('ws://' + host + ':' + ws_port + '/');
+    ws.onmessage = messageHandler;
   });
 });
