04752d3859d3e240bf985f4a8cb18a8dc59b37fd
Franz Gutsch <franz.balthasar-git@gmail.com>
2017-03-17 09:38:55 +0100

    Update plots programmatically.

--- pyodine/gui/pyodine-gui.js
+++ pyodine/gui/pyodine-gui.js
@@ -6,13 +6,13 @@ jQuery(function(){
     return new Promise(resolve => setTimeout(resolve, ms));
   }
 
-  async function createDummyStreamer(plotElm, title) {
+  async function createDummyStreamer(plotElm) {
     var temp = {
       y: [0],
       mode: 'lines'
     };
     var data = [temp];
-    var layout = {title: title};
+    var layout = {title: "Dummy Data!"};
     var plt = document.getElementById('sup1_ld_temp');
     var display_range = 100;
 
@@ -25,7 +25,6 @@ jQuery(function(){
   }
 
   function updatePlot(plot_div, times, values, display_count=40) {
-
     if (typeof(plot_div.data) == 'object') {  // Plot exists, update it.
       Plotly.extendTraces(plot_div, {x: [times], y: [values]}, [0], display_count);
     } else {  // Create new plot.
@@ -42,6 +41,26 @@ jQuery(function(){
         y: values
       }];
       Plotly.newPlot(plot_div, data, layout);
+    }
+  }
+
+  function updateAllPlots(new_values_obj) {
+
+    // Gather available plot areas.
+    let available_plots = new Map();
+    $('div.plot',DOM_SCOPE).each(function(){
+      available_plots.set(this.id, this);
+    });
+
+    // Update them.
+    for (const [id, data] of new_values_obj) {
+      if (id in available_plots.keys()) {
+        console.assert(data.x.length > 0);
+        console.assert(data.y.length > 0);
+        updatePlot(available_plots[id], data.x, data.y);
+      } else {
+        console.log(`No graph available to plot data of type ${id}`)
+      }
     }
   }
 
