7506f05663660268de1727c04acf69620fca9e06
Franz Gutsch <franz.balthasar-git@gmail.com>
2017-03-20 15:27:46 +0100

    Basic WS<->RS232 forwarder done.

--- pyodine/gui/serial_websocket.py
+++ pyodine/gui/serial_websocket.py
@@ -1,11 +1,61 @@
 """A service that passes data between a serial connection and a websocket.
 """
-import typing
+import asyncio
 import serial
-import websockets
+import logging
+from ..transport.websocket_server import WebsocketServer
+
+LOGGER = logging.getLogger('pyodine.gui.serial_websocket')
 
 
 class SerialWebsocket():
 
-    def __init__():
-        pass
+    def __init__(self, ws_port: int, serial_device: str):
+        """Do also run async_init()."""
+        LOGGER.debug("Creating instance...")
+        self._ws_port = ws_port
+        self._serial = serial.Serial(port=serial_device, baudrate=19200)
+        self._ws_server = None  # type: WebsocketServer
+        LOGGER.debug("Created instance.")
+
+    async def async_init(self):
+        self._ws_server = WebsocketServer(
+                port=self._ws_port, received_msg_callback=self._forward_reply)
+        await self._ws_server.async_init()
+
+    def start_server(self):
+        LOGGER.info("Starting server.")
+        asyncio.ensure_future(self._server())
+
+    def _forward_reply(self, message: str):
+        n_bytes = message.encode()
+        n_transmitted_bytes = self._serial.write(message.encode)
+        if (n_transmitted_bytes == n_bytes):
+            LOGGER.debug("Transmitted Message: %s", message)
+        else:
+            LOGGER.warning("Error transmitting Message.")
+
+    async def _server(self):
+        while True:
+            if (self._serial.in_waiting > 0):
+                data = self._serial.read(1)
+                data += self._serial.read(self._serial.in_waiting)
+                message = data.decode(encoding='utf-8', errors='ignore')
+                LOGGER.debug("Received message: %s", message)
+                asyncio.ensure_future(self._ws_server.publish(message))
+            else:
+                await asyncio.sleep(0.1)  # OPTIMIZE: Do this elegantly.
+
+
+async def launch():
+    ws = SerialWebsocket(ws_port=56321, serial_device='/dev/ttyUSB0')
+    await ws.async_init()
+    ws.start_server()
+
+if __name__ == '__main__':
+    print('foo')
+    logging.basicConfig(level=logging.DEBUG)
+    LOGGER.info("foo")
+    asyncio.ensure_future(launch())
+    loop = asyncio.get_event_loop()
+    loop.run_forever()
--- pyodine/transport/websocket_server.py
+++ pyodine/transport/websocket_server.py
@@ -1,4 +1,7 @@
 """A simple websocket server.
+
+It manages a list of subscribers to whom it can publish data.
+It can forward received messages to a callback handler.
 """
 
 import asyncio
@@ -10,7 +13,7 @@ LOGGER = logging.getLogger("pyodine.transport.websocket_server")
 
 class WebsocketServer:
 
-    def __init__(self, port: int=56320):
+    def __init__(self, port: int=56320, received_msg_callback=None):
         """Mustn't be run alone. Be sure to await the async_init() coroutine
         afterwards.
         The default port number is inspired by the 56(32)-0 iodine hyperfine
@@ -19,15 +22,17 @@ class WebsocketServer:
         """
         self.port = port
         self.subscribers = set()
-        LOGGER.info("Creating JsonWs instance. Do call the async_init() fcn.")
+        self._rcv_callback = received_msg_callback
+        LOGGER.info("Creating WebsocketServer instance. Do call the async_init() fcn.")
 
     async def async_init(self) -> None:
+        LOGGER.info("async_init() called.")
         LOGGER.info("Starting server on port %d.", self.port)
         asyncio.ensure_future(websockets.serve(self._register_subscriber,
                                                port=self.port))
 
     async def publish(self, data: str) -> None:
-        LOGGER.debug("Trying to publish: " + data)
+        LOGGER.debug("Trying to publish: %s", data)
         if len(self.subscribers) > 0:
 
             # Send data to every subscriber.
