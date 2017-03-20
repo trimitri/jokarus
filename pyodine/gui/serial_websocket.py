61676b14f41e6af28a80606546f9fe905f54d367
Franz Gutsch <franz.balthasar-git@gmail.com>
2017-03-20 12:32:19 +0100

    Started WS/RS232 forwarder service.

---
+++ pyodine/gui/serial_websocket.py
@@ -0,0 +1,11 @@
+"""A service that passes data between a serial connection and a websocket.
+"""
+import typing
+import serial
+import websockets
+
+
+class SerialWebsocket():
+
+    def __init__():
+        pass
