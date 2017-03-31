8c3cd8833f96e80810fefeced3ec1cbb7b48fb5b
Franz Gutsch <franz.balthasar-git@gmail.com>
2017-03-31 18:22:06 +0200

    Evolved general project description.

--- pyodine/__init__.py
+++ pyodine/__init__.py
@@ -15,20 +15,9 @@ Ein kurzer Überblick über die geplante Projektstruktur
   siehe unten)
 - :doc:`pyodine.controller`: Eigentliche Steuerung des Programmflusses,
   Finden-und-Locken-Mechanismus etc.
-- :doc:`pyodine.comm`: Tools für die Kommunikation zwischen verschiedenen
-  Programmteilen (z.B. Pyodine-Server in der Rakete und GUI am Boden)
+- :doc:`pyodine.transport`: Tools für externe Kommunikation, Protokolle
 - :doc:`pyodine.test`: Test-Module, die automatisiert alle wesentlichen
   Funktionen aller anderen Module testen ("test based development")
-
-Von der Sache her stelle ich mir das so vor: Am Experiment (also auf dem
-Pokini) läuft das Hauptprogramm. Es besteht aus mehreren Subprozessen (oder
-Threads, noch nicht entschieden), die für die einzelnen Subsysteme des Aufbaus
-verantwortlich sind. Dieses Hauptprogromm ("Pyodine Server") hat keine
-grafische Oberfläche.  Es stellt sein Interface stattdessen über einen
-RPC-Service (vermutlich XML-RPC, evtl JSON-RPC) zur Verfügung.  Die grafische
-Oberfläche wird vermutlich einfach eine HTML-Datei mit div. kleinen JS-Skripten
-sein (um Schieber, Knöpfe, Signalplots etc. anzuzeigen), die als RPC-Client mit
-dem Server kommuniziert.
 
 Begründungen zu einzelnen Designideen
 -------------------------------------
@@ -38,10 +27,6 @@ Warum Server und Client so strikt trennen?
     gehalten werden. Wenn es Probleme mit der Oberfläche gibt, kann der Server
     einfach weiterlaufen. Die Benutzeroberfläche kann beliebig oft verbunden
     oder getrennt werden.
-
-Warum RPC?
-    Ich habe mir einige Protokolle angeschaut und alle anderen wären vermutlich
-    overkill.
 
 Warum eine HTML-Oberfläche?
     HTML ist simpel und allgegenwärtig. Das hat den Vorteil, dass auf dem
