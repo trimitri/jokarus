3da055e0f918b44f4f617b558c8b434e804e9db1
Franz Gutsch <franz.balthasar-git@gmail.com>
2017-04-03 18:27:28 +0200

    TemperatureRamp class at least doesn't hurt.

--- pyodine/controller/subsystems.py
+++ pyodine/controller/subsystems.py
@@ -4,6 +4,7 @@ This is an interface to the actual things connected to each port of each
 subsystem.
 """
 import logging
+import time
 from typing import Dict, List, Tuple, Union
 from ..drivers import menlo_stack
 from .temperature_ramp import TemperatureRamp
@@ -24,7 +25,7 @@ class Subsystems:
 
     def __init__(self) -> None:
         self._menlo = None  # type: menlo_stack.MenloStack
-        self._temp_ramps = Dict()  # type: Dict[int, TemperatureRamp]
+        self._temp_ramps = dict()  # type: Dict[int, TemperatureRamp]
         self._init_temp_ramps()
         # self._dds = None
         # self._daq = None
@@ -57,28 +58,30 @@ class Subsystems:
         LOGGER.debug("Setting diode current of unit %s to %s mA",
                      unit_name, milliamps)
         if (unit_name in OSC_UNITS
-                and type(milliamps) is float
+                and isinstance(milliamps, float)
                 and milliamps > 0):
             self._menlo.set_current(OSC_UNITS[unit_name], milliamps)
         else:
             LOGGER.error("Illegal setting for diode current.")
 
     def set_temp(self, unit_name, celsius: float) -> None:
+        """Set the target temp. for the temperature ramp."""
         LOGGER.debug("Setting target temp. of unit %s to %s°C",
                      unit_name, celsius)
-        if unit_name in OSC_UNITS and type(celsius) is float:
-            self._menlo.set_temp(OSC_UNITS[unit_name], celsius)
+        if unit_name in OSC_UNITS and isinstance(celsius, float):
+            ramp = self._temp_ramps[OSC_UNITS[unit_name]]
+            ramp.target_temperature = celsius
         else:
             LOGGER.error("Illegal setting for temperature setpoint.")
 
     def switch_tec(self, unit_name: str, on: bool) -> None:
         if unit_name in OSC_UNITS:
-            if type(on) is bool:
+            if isinstance(on, bool):
                 self._menlo.switch_tec(OSC_UNITS[unit_name], on)
 
     def switch_ld(self, unit_name: str, on: bool) -> None:
         if unit_name in OSC_UNITS:
-            if type(on) is bool:
+            if isinstance(on, bool):
                 self._menlo.switch_ld(OSC_UNITS[unit_name], on)
 
     def get_full_set_of_readings(self) -> Dict[str, Buffer]:
@@ -99,23 +102,18 @@ class Subsystems:
         data['mo_tec_enabled'] = self._menlo.is_tec_enabled(1)
         data['mo_temp'] = self._menlo.get_temperature(1)
         data['mo_temp_set'] = self._menlo.get_temp_setpoint(1)
-        # data['mo_temp_ok'] = self._menlo.is_temp_ok(1)
+        data['mo_temp_ramp_set'] = self._wrap_into_buffer(
+            self._temp_ramps[1].target_temperature)
+        data['mo_temp_ramp_active'] = self._wrap_into_buffer(
+            self._temp_ramps[1].is_running)
+        data['mo_temp_ok'] = self._menlo.is_temp_ok(1)
         data['mo_tec_current'] = self._menlo.get_tec_current(1)
-
-        data['pa_enabled'] = self._menlo.is_current_driver_enabled(2)
-        data['pa_current'] = self._menlo.get_diode_current(2)
-        data['pa_current_set'] = self._menlo.get_diode_current_setpoint(2)
-        data['pa_tec_enabled'] = self._menlo.is_tec_enabled(2)
-        data['pa_temp'] = self._menlo.get_temperature(2)
-        data['pa_temp_set'] = self._menlo.get_temp_setpoint(2)
-        # data['pa_temp_ok'] = self._menlo.is_temp_ok(2)
-        data['pa_tec_current'] = self._menlo.get_tec_current(2)
 
         return data
 
     def _init_temp_ramps(self) -> None:
         """Initialize one TemperatureRamp instance for every TEC controller."""
-        for unit in OSC_UNITS.values():
+        for name, unit in OSC_UNITS.items():
             def getter() -> float:
                 """Get the most recent temperature reading from MenloStack."""
 
@@ -131,4 +129,25 @@ class Subsystems:
                 self._menlo.set_temp((lambda: unit)(), temp)
 
             self._temp_ramps[unit] = TemperatureRamp(get_temp_callback=getter,
-                                                     set_temp_callback=setter)
+                                                     set_temp_callback=setter,
+                                                     name=name)
+
+    @staticmethod
+    def _wrap_into_buffer(value: Union[MenloUnit, bool]) -> Buffer:
+        if isinstance(value, bool):
+            return [(time.time(), 1 if value else 0)]  # bool is no MenloUnit
+
+        if isinstance(value, float):
+            return [(time.time(), float(value))]  # float(): make mypy happy
+
+        if isinstance(value, int):
+            return [(time.time(), int(value))]  # int(): make mypy happy
+
+        if value is None:
+            # Don't throw an error here, as None might just be an indication
+            # that there isn't any data available yet.
+            return []
+
+        LOGGER.error("Type %s is not convertible into a MenloUnit.",
+                     type(value))
+        return []
--- pyodine/controller/temperature_ramp.py
+++ pyodine/controller/temperature_ramp.py
@@ -3,43 +3,73 @@
 This is necessary, if a temperature controller does not include the feature to
 set a limit to the temperature gradient.
 """
-from typing import Callable
+import asyncio
 import inspect
 import logging
 import math
+import time
+from typing import Callable
 
 LOGGER = logging.getLogger("pyodine.controller.temperature_ramp")
 LOGGER.setLevel(logging.DEBUG)
 
+# The interval at which the transitional setpoint gets updated (in seconds). It
+# is advisable to use some weird number here to make sure that asyncio tasks
+# are evenly spaced in time.
+UPDATE_INTERVAL = 11.73
 
+# The distance (in Kelvin) between setpoint and temperature reading that is
+# acceptable during normal operation of the ramp. Do not set this to zero, as
+# it might significantly slow down operation or even impede it.
+ACCEPTABLE_OFFSET = 1.0
+
+
+# pylint: disable=too-many-instance-attributes
 class TemperatureRamp:
     """A stateful executor for a limited-gradient temperature ramp."""
 
     # Pylint doesn't recognize typing's subscriptable metaclasses.
     # pylint: disable=unsubscriptable-object
     def __init__(self, get_temp_callback: Callable[[], float],
-                 set_temp_callback: Callable[[float], None]) -> None:
+                 set_temp_callback: Callable[[float], None],
+                 name: str) -> None:
 
-        # Check and store getter callback.
+        self.name = name  # descriptive name for this instance
+
+        # Validate and store getter callback.
         sig = inspect.signature(get_temp_callback)
         if sig.return_annotation is float and not sig.parameters:
             self._get_temp = get_temp_callback
         else:
+            LOGGER.debug(sig.return_annotation)
+            LOGGER.debug(sig.parameters)
             raise TypeError("Provide a type-annotated callback of proper"
                             "signature.", get_temp_callback)
 
-        # Check and store setter callback.
+        # Validate and store setter callback.
         sig = inspect.signature(set_temp_callback)
         param_types = list(sig.parameters.values())
-        if len(param_types) == 1 and param_types[0] is float:
+        if len(param_types) == 1 and param_types[0].annotation is float:
             self._set_temp = set_temp_callback
         else:
+            LOGGER.debug(param_types)
             raise TypeError("Provide a type-annotated callback of proper"
                             "signature.", set_temp_callback)
 
         self._target = None  # type: float # Target temperature
         self._max_grad = 1. / 60.  # Maximum temperature gradient (1K/min)
-        self._current_setpt = None  # type: float # Current internal setpoint
+
+        # Current transitional setpoint (in deg. Celsius).
+        self._current_setpt = None  # type: float
+
+        # Previous transitional setpoint (in deg. Celsius).
+        self._prev_setpt = None  # type: float
+
+        # Last update of transitional setpoint was at that time (unix
+        # timestamp).
+        self._prev_time = None  # type: float
+
+        self._keep_running = False  # Is used to break the loop when runnning.
 
     @property
     def target_temperature(self) -> float:
@@ -67,13 +97,68 @@ class TemperatureRamp:
         else:
             LOGGER.error("Illegal value for temp. gradient: %s", gradient)
 
+    @property
+    def is_running(self) -> bool:
+        """Is the temperature control currently running?"""
+        return self._keep_running
+
+    @property
+    def temperature(self) -> float:
+        return self._get_temp()
+
     def start_ramp(self) -> None:
-        """Start pediodically setting the temp. setpoint."""
+        """Start/resume pediodically setting the temp. setpoint."""
         LOGGER.debug("Starting to ramp temperature.")
-        # TODO: implement!
+
+        # Create a "runner" coroutine and then schedule it for running.
+        async def run_ramp() -> None:
+            while self._keep_running:
+                self._update_transitional_setpoint()
+                await asyncio.sleep(UPDATE_INTERVAL)
+        self._keep_running = True
+        asyncio.ensure_future(run_ramp())
 
     def pause_ramp(self) -> None:
         """Stop setting new temp. setpoints, stay at current value."""
         LOGGER.debug("Pausing temperature ramp, staying at %s.",
                      self._current_setpt)
-        # TODO: implement!
+        self._keep_running = False
+        self._prev_setpt = None
+        self._prev_time = None
+
+    def _update_transitional_setpoint(self) -> None:
+        """Set a new intermediate setpoint if the thermal load is following.
+        """
+        # Exit prematurely if we're there already.
+        if self._prev_setpt == self._target:
+            LOGGER.debug("Currently at target temperature.")
+            return
+
+        # Are we close enough to the current setpoint to continue?
+        if abs(self._get_temp() - self._current_setpt) < ACCEPTABLE_OFFSET:
+            now = time.time()
+            sign = -1 if self._target < self._get_temp() else 1
+            next_setpt = self._prev_setpt + (
+                (now - self._prev_time) * sign * self._max_grad)
+
+            # Prevent overshoot and set target temperature directly instead.
+            if ((self._prev_setpt - self._target) * (next_setpt - self._target)
+                    < 0):
+                next_setpt = self._target
+                LOGGER.info("Reached target temperature.")
+
+            # Advance time.
+            self._prev_setpt = self._current_setpt
+            self._current_setpt = next_setpt
+            self._prev_time = now
+
+            # Actually set the new temperature in hardware.
+            self._set_temp(self._current_setpt)
+
+            LOGGER.debug("New transitional setpoint is spaced %s Kelvin from"
+                         "last one.", self._current_setpt - self._prev_setpt)
+        else:
+            # Don't do anything and wait until next invocation for the object
+            # temperature to settle.
+            LOGGER.warning("Thermal load didn't follow ramp. Delaying ramp"
+                           "continuation by %s seconds.", UPDATE_INTERVAL)
