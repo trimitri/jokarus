3c90291a5fb60f552ea22380503fc5fbf9e00738
Franz Gutsch <franz.balthasar-git@gmail.com>
2017-04-04 10:03:25 +0200

    Cosmetics on instr.handler, fix for temp. ramp.

--- pyodine/controller/instruction_handler.py
+++ pyodine/controller/instruction_handler.py
@@ -2,42 +2,46 @@
 """
 import json
 import logging
+from typing import Callable, Dict
 from .subsystems import Subsystems
 from .interfaces import Interfaces
+
+LegalCall = Callable[..., None]
 
 LOGGER = logging.getLogger("pyodine.controller.instruction_handler")
 LOGGER.setLevel(logging.DEBUG)
 
 
+# pylint: disable=too-few-public-methods
+# The main method is the single functionality of this class.
 class InstructionHandler:
+    """This class receives external commands and forwards them."""
 
     def __init__(self, subsystem_controller: Subsystems,
                  interface_controller: Interfaces) -> None:
         self._subs = subsystem_controller
         self._face = interface_controller
-        self.LEGAL_METHODS = {
-                'set_mo_current': lambda c: self._subs.set_current(
-                    'mo', float(c)),
-                'set_mo_temp': lambda t: self._subs.set_temp('mo', float(t)),
-                'set_pa_current': lambda c: self._subs.set_current(
-                    'pa', float(c)),
-                'set_pa_temp': lambda t: self._subs.set_temp('pa', float(t)),
-                'switch_tec': self._subs.switch_tec,
-                'switch_ld': self._subs.switch_ld,
-                'setflag': self._face.set_flag,
-                }
+
+        self._methods = {
+            'set_mo_current': lambda c: self._subs.set_current('mo', float(c)),
+            'set_mo_temp': lambda t: self._subs.set_temp('mo', float(t)),
+            'set_pa_current': lambda c: self._subs.set_current('pa', float(c)),
+            'set_pa_temp': lambda t: self._subs.set_temp('pa', float(t)),
+            'switch_tec': self._subs.switch_tec,
+            'switch_ld': self._subs.switch_ld,
+            'setflag': self._face.set_flag}  # type: Dict[str, LegalCall]
 
     def handle_instruction(self, message: str) -> None:
         try:
             container = json.loads(message)
             method = container['data']['method']
             arguments = container['data']['args']
-            if method in self.LEGAL_METHODS:
-                if type(arguments) is list:
+            if method in self._methods:
+                if isinstance(arguments, list):
                     try:
                         LOGGER.debug("Calling method %s with arguments: %s",
                                      method, arguments)
-                        (self.LEGAL_METHODS[method])(*arguments)
+                        (self._methods[method])(*arguments)
                     except TypeError:
                         LOGGER.error("Wrong type/number of arguments.")
                 else:
