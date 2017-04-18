86b4b5b79963fe40068b44a9a4666ad201d1acb3
Franz Gutsch <franz.balthasar-git@gmail.com>
2017-04-18 10:51:51 +0200

    Created a dummy "Plotter" class for GUI.

---
+++ pyodine/gui/plotter.js
@@ -0,0 +1,18 @@
+/* eslint-env es6, browser, jquery */
+/* eslint no-alert: "off" */
+/* global CanvasJS */
+
+class Plotter {  // eslint-disable-line no-unused-vars
+  constructor() {
+    this.foo = 'bar';
+  }
+
+  static sayHello() {
+    alert('Hello World!');
+  }
+
+  sayBar() {
+    alert(this.foo);
+  }
+}
+
