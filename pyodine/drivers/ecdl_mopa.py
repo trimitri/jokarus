da0c4ff65071202618ba82d77d17ae98ed4e6db8
Franz Gutsch <franz.balthasar-git@gmail.com>
2017-05-19 12:33:38 +0200

    ecdl_mopa module is implemented but untested.
    Towards #28.

--- pyodine/drivers/ecdl_mopa.py
+++ pyodine/drivers/ecdl_mopa.py
@@ -24,11 +24,31 @@ MILAS = MopaSpec(mo_max=200, mo_seed_threshold=50,
                  pa_max=1500, pa_transparency=200, pa_backfire=300)
 
 
+class CallbackError(RuntimeError):
+    """There was an error executing one of the callbacks.
+
+    As the callback functions are supposed to not raise any exceptions, this is
+    most definitely an indicator for a necessary subsystem reset.
+    """
+    pass
+
+
 class EcdlMopa:  # pylint: disable=too-many-instance-attributes
     """A wrapper for the two current drivers needed for a MOPA laser.
 
     It makes sure that all requested currents stay within the non-trivial MOPA
     operation envelope.
+
+    This class does not, however, make sure that the requested currents are
+    actually reached. As the provided callbacks are not allowed to throw any
+    exceptions, it is up to the user to check for success of their request.
+    This decision was made because it takes time to evaluate the success of
+    setting a current.
+
+    All methods may raise a ecdl_mopa.CallbackError if there is a problem
+    executing a callback. Mutators (incl. en/disable!) may additionally raise
+    a ValueError in case the current operating regime forbids the requested
+    action.
     """
     def __init__(self,  # pylint: disable=too-many-arguments
                  # pylint: disable=unsubscriptable-object
@@ -52,96 +72,190 @@ class EcdlMopa:  # pylint: disable=too-many-instance-attributes
         self._enable_pa = enable_pa_callback
         self._disable_pa = disable_pa_callback
 
-    @property
-    def mo_current(self) -> float:
+    def get_mo_current(self) -> float:
         """The master oscillator laser diode current in milliamps."""
         try:
             return self._get_mo_current()
         except:  # pylint: disable=bare-except
             LOGGER.exception("Error while executing get_mo_current_callback.")
+            raise CallbackError("Callback provided to %s raised an exception!",
+                                __name__)
+
+        # Is never reached.
         return float('nan')
 
-    @mo_current.setter
-    def mo_current(self, milliamps: float) -> None:
-        try:
+    def set_mo_current(self, milliamps: float) -> None:
+        """Set the current setpoint for the master oscillator laser diode.
+
+        :raises ValueError: The given value must not be applied in the current
+                            operating regime.
+        :raises CallbackError:
+        """
+        try:  # type check
             current = float(milliamps)
         except (TypeError, ValueError):
-            LOGGER.error("Couldn't parse passed current.")
+            LOGGER.error("Couldn't parse passed current. Doing nothing.")
             return
 
-        # FIXME: Check all if's for NaN/inf safety
+        # NOTE: The following conditions are all formulated in a way that
+        # ensures failure in case of NaN/inf occurrences.
 
         # Don't exceed maximum diode rating.
-        if current > self._spec.mo_max:
-            LOGGER.warning("Requested current exceeds specified maximum "
-                           "current for master oscillator.")
-            return
+        if not current < self._spec.mo_max:
+            raise ValueError("Requested current exceeds specified maximum "
+                             "current for master oscillator.")
 
         # Don't dump (more) energy into the PA if it is not transparent.
-        if current > 0 and self.pa_current < self._spec.pa_transparency:
-            LOGGER.warning("Refusing to increase MO current, as PA is not "
-                           "transparent.")
-            return
+        # Use "not" flavour of conditions to make sure that Nan/Inf lead to
+        # failure.
+        if (current != 0 and
+                not self.get_pa_current() >= self._spec.pa_transparency):
+            raise ValueError("Refusing to increase MO current, as PA is not "
+                             "transparent.")
 
         # Don't decrease MO current below lasing level if PA is in a regime
         # allowing backfire. Otherwise powerful backfire resulting from
         # spontaneous emission might occur.
-        if (current < self._spec.mo_seed_threshold
-                and self.pa_current >= self._spec.pa_backfire):
-            LOGGER.warning("Refusing to decrease MO current below seeding "
-                           "level, as PA is in a backfire regime.")
-            return
+        if (not current >= self._spec.mo_seed_threshold
+                and not self.get_pa_current() < self._spec.pa_backfire):
+            raise ValueError("Refusing to decrease MO current below seeding "
+                             "level, as PA is in a backfire regime.")
 
         # If we got here, everything should be alright. Try to actually set the
         # new current.
         try:
             self._set_mo_current(current)
         except:  # pylint: disable=bare-except
-            LOGGER.exception("Error excecuting get_mo_callback.")
+            LOGGER.exception("Error excecuting get_mo_callback:")
+            raise CallbackError("Callback provided to %s raised an exception!",
+                                __name__)
 
-    @property
-    def pa_current(self) -> float:
+    def get_pa_current(self) -> float:
         """The laser power amplifier current in milliamps."""
         try:
             return self._get_pa_current()
         except:  # pylint: disable=bare-except
             LOGGER.exception("Error while executing get_pa_current_callback.")
+            raise CallbackError("Callback provided to %s raised an exception!",
+                                __name__)
+
+        # We never get here.
         return float('nan')
 
-    @pa_current.setter
-    def pa_current(self, milliamps: float) -> None:
-        pass  # FIXME
+    def set_pa_current(self, milliamps: float) -> None:
+        """Set the current setpoint for the laser power amplifier.
 
-    def pa_disable(self, force: bool = False) -> None:
+        :raises ValueError: The given value must not be applied in the current
+                            operating regime.
+        :raises CallbackError:
+        """
+        try:  # type check
+            current = float(milliamps)
+        except (TypeError, ValueError):
+            LOGGER.error("Couldn't parse passed current. Doing nothing.")
+            return
+
+        # NOTE: The following conditions are all formulated in a way that
+        # ensures failure in case of NaN/inf occurrences.
+
+        # Don't exceed maximum diode rating.
+        if not current <= self._spec.pa_max:
+            raise ValueError("Requested current exceeds specified maximum "
+                             "current for power amplifier.")
+
+        # Don't go below PA transparency current while MO is seeding. This
+        # avoids dumping energy into the PA.
+        if (not current >= self._spec.pa_transparency and
+                not self.get_mo_current() < self._spec.mo_seed_threshold):
+            raise ValueError("Refusing to go below PA transparency current, "
+                             "as MO is still seeding.")
+
+        # Don't increase PA current to backfire-prone current levels if MO is
+        # not seeding. Otherwise powerful backfire resulting from spontaneous
+        # emission might occur and damage internal components.
+        if (not current < self._spec.pa_backfire and
+                not self.get_mo_current() >= self._spec.mo_seed_threshold):
+            raise ValueError("Refusing to enter backfire-prone PA current "
+                             "range, as mo is not seeding.")
+
+        # If we got here, everything should be alright. Try to actually set the
+        # new current.
+        try:
+            self._set_pa_current(current)
+        except:  # pylint: disable=bare-except
+            LOGGER.exception("Error excecuting get_mo_callback:")
+            raise CallbackError("Callback provided to %s raised an exception!",
+                                __name__)
+
+    def disable_mo(self, force: bool = False) -> None:
+        """Switch off master oscillator if safe. Use force to skip checks.
+
+        :raises ValueError: Turning off MO not allowable in current regime.
+                            This is only raised if "force" is not used.
+        :raises CallbackError:
+        """
+        if not force:
+            self.set_mo_current(0)
+
+        # Zeroing the current is allowable in the current regime or was forced.
+        # Switch off now.
+        try:
+            self._disable_mo()
+        except:  # pylint: disable=bare-except
+            LOGGER.exception("Error excecuting disable_mo_callback:")
+            raise CallbackError("Callback provided to %s raised an exception!",
+                                __name__)
+
+    def disable_pa(self, force: bool = False) -> None:
         """Switch off power amplifier if safe. Use force to skip checks.
         """
-        if self.mo_current < self._spec.mo_seed_threshold or force:
-            try:
-                self._disable_pa()
+        if not force:
+            self.set_pa_current(0)
 
-            # We use the ultimate exception catcher here, as we're dealing
-            # with a generally unknown callback.
-            except:  # pylint: disable=bare-except
-                LOGGER.exception("Error while executing disable_pa_callback.")
-        else:
-            LOGGER.error("Can not disable PA, as MO is running above seed "
-                         "threshold and would dump into PA otherwise.")
-            LOGGER.info("Consider using \"force\" attribute if something went "
-                        "wrong and PA needs to be switched off regardless.")
+        # Zeroing the current is allowable in the current regime or was forced.
+        # Switch off now.
+        try:
+            self._disable_pa()
+        except:  # pylint: disable=bare-except
+            LOGGER.exception("Error excecuting disable_pa_callback.")
+            raise RuntimeError("Failed to execute callback.")
 
-    def mo_disable(self, force: bool = False) -> None:
-        """Switch off master oscillator if safe. Use force to skip checks.
+    def enable_mo(self, force: bool = False) -> None:
+        """Switch on master oscillator (setting it to 0 mA).
+
+        Use "force" to skip all sanity checks and switch it on. This skips
+        setting the current to 0 mA!
+
+        :raises ValueError: Current regime doesn't allow setting MO to 0 mA. Is
+                            only raised if "force" is not used.
+        :raises CallbackError:
         """
-        if self.pa_current < self._spec.pa_backfire or force:
-            try:
-                self._disable_mo()
+        if not force:
+            self.set_mo_current(0)
 
-            # We use the ultimate exception catcher here, as we're dealing
-            # with a generally unknown callback.
-            except:  # pylint: disable=bare-except
-                LOGGER.exception("Error while executing disable_mo_callback.")
-        else:
-            LOGGER.error("Can not disable PA, as MO is running above seed "
-                         "threshold and would dump into PA otherwise.")
-            LOGGER.info("Consider using \"force\" attribute if something went "
-                        "wrong and PA needs to be switched off regardless.")
+        try:
+            self._enable_mo()
+        except:  # pylint: disable=bare-except
+            LOGGER.exception("Error while executing enable_mo_callback.")
+            raise CallbackError("Callback provided to %s raised an exception!",
+                                __name__)
+
+    def enable_pa(self, force: bool = False) -> None:
+        """Switch on power amplifier (setting it to 0 mA).
+
+        Use "force" to skip all sanity checks and switch it on. This skips
+        setting the current to 0 mA!
+
+        :raises ValueError: Current regime doesn't allow setting PA to 0 mA. Is
+                            only raised if "force" is not used.
+        :raises CallbackError:
+        """
+        if not force:
+            self.set_pa_current(0)
+
+        try:
+            self._enable_pa()
+        except:  # pylint: disable=bare-except
+            LOGGER.exception("Error while executing enable_pa_callback.")
+            raise CallbackError("Callback provided to %s raised an exception!",
+                                __name__)
