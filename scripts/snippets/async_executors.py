fbe2132fa10dc1e2c5f973eafcdceb4b1bd103cd
Franz Gutsch <franz.balthasar-git@gmail.com>
2017-09-28 15:37:12 +0200

    Removed explicit Executor, added cancelling.

--- scripts/snippets/async_executors.py
+++ scripts/snippets/async_executors.py
@@ -1,13 +1,14 @@
 """Test module for asyncio + Executors"""
 
 import asyncio
-from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
+import functools
 import time
 
-# EXECUTOR = ProcessPoolExecutor()
-EXECUTOR = ThreadPoolExecutor(thread_name_prefix="foothread")
 LOOP = asyncio.get_event_loop()  # type: asyncio.AbstractEventLoop
 
+# This is a flag that gets passed around. We can't use a plain bool here, as it
+# is immutable.
+FOO = [False]
 
 def busy_wait(seconds):
     now = time.time()
@@ -15,9 +16,12 @@ def busy_wait(seconds):
         pass
 
 
-def print_a():
+def print_a(magic_flag):
+    """This one can be cancelled."""
     times = 1
-    while True:
+    # We need to poll an external flag here, as -- due to this not being an
+    # asyncio coroutine -- it is otherwise not cancellable!
+    while not magic_flag[0]:
         print("A printed {} times.".format(times))
         times += 1
         busy_wait(1)
@@ -41,17 +45,25 @@ def print_c():
 
 async def do_nothing():
     initial_slip = time.time() - asyncio.get_event_loop().time()
-    while True:
+
+    async def dostuff():
         await asyncio.sleep(1)
         print("Event loop still alive. async time: {} Time slip: {}".format(
             asyncio.get_event_loop().time(),
             time.time() - asyncio.get_event_loop().time() - initial_slip))
 
+    for i in range(5):
+        await dostuff()
+    FOO[0] = True
+    while True:
+        await dostuff()
+
 
 if __name__ == '__main__':
-    asyncio.ensure_future(LOOP.run_in_executor(EXECUTOR, print_a))
-    asyncio.ensure_future(LOOP.run_in_executor(EXECUTOR, print_b))
-    asyncio.ensure_future(LOOP.run_in_executor(EXECUTOR, print_c))
+
+    asyncio.ensure_future(LOOP.run_in_executor(None, functools.partial(print_a, FOO)))
+    asyncio.ensure_future(LOOP.run_in_executor(None, print_b))
+    asyncio.ensure_future(LOOP.run_in_executor(None, print_c))
     LOOP.run_until_complete(do_nothing())
 
     raise RuntimeError("You shouldn't have come here.")
