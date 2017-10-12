a36c0fa2f8cff2239a7a646e2790cf4fb8ff15d8
Jokarus user on Jokarus server <gutsch@physik.hu-berlin.de>
2017-10-12 15:15:10 +0200

    Added a simple websocket tester module.
    Helped with finding #143.

---
+++ pyodine/test/websockets_sanity.py
@@ -0,0 +1,18 @@
+"""Test the `websockets` package."""
+
+import asyncio
+
+import websockets
+
+URL = 'ws://menlostack:8000'
+SILENT_URL = 'ws://echo.websocket.org'
+
+
+async def open_and_wait():
+    _ = await websockets.connect(URL)
+    while True:
+        await asyncio.sleep(3)
+        print("Sleeping.")
+
+if __name__ == '__main__':
+    asyncio.get_event_loop().run_until_complete(open_and_wait())
