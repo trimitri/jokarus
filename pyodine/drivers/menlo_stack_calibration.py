3e69a4b7c348a8d3106c8993cb711f6e2b10b463
Franz Gutsch <franz.balthasar-git@gmail.com>
2017-11-07 09:24:06 +0100

    Added MO calibration.

--- pyodine/drivers/menlo_stack_calibration.py
+++ pyodine/drivers/menlo_stack_calibration.py
@@ -2,7 +2,7 @@
 """
 import enum
 
-class OscCard(enum.IntEnum):
+class OscCard(enum.Enum):
     """All oscillator supply cards that where actually produced."""
     OSC1A = 4  # Card number 1 from stack A
     OSC2A = 5  # Card number 2 from stack A
@@ -13,5 +13,12 @@ class OscCard(enum.IntEnum):
     OSC3B = 41216
     OSC4B = 11216
 
-LD_CURRENT_SETTER = {card: lambda x: x for card in OscCard}
-LD_CURRENT_SETTER[OscCard.OSC1A] = lambda x: x
+# Using linear fits on data acquired in a calibration run, we may improve the
+# current driver accuracy. Data is on JOKARUS share.
+LD_CURRENT_SETTER = {card: lambda I: I for card in OscCard}
+"""Translate a desired current setpoint into a value to send to Menlo."""
+LD_CURRENT_SETTER[OscCard.OSC1A] = lambda I: 1.0430516711750952 * I + 10.07060657466415
+
+LD_CURRENT_GETTER = {card: lambda x: x for card in OscCard}
+"""Estimate the actual current given a menlo current reading."""
+LD_CURRENT_GETTER[OscCard.OSC1A] = lambda I: 1.021324354657688 * I + 17.542087542087543
