62dfdad0a0dc2ebe67c8f7726c18e96d797dd06d
Franz Gutsch <franz.balthasar-git@gmail.com>
2017-11-09 10:33:00 +0100

    Started to aggregate constants.

---
+++ pyodine/constants.py
@@ -0,0 +1,14 @@
+"""Various constants determining the peculiarities and behaviour of JOKARUS.
+"""
+
+_MIOB_mv_K = -95.821785132407868
+"""Measured detuning of laser detuning (in lockbox mV) vs MiOB temp in K.
+
+Acquired 8/11/17 from a fit to a measurement taken 7/11/17.
+"""
+
+_LOCKBOX_mA_mV = 0.00079119970889856704
+"""Actual LD current change in mA per applied ramp input (mV) into lockbox.
+
+Fitted to the data taken on 23/10/17.
+"""
