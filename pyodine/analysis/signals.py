432dca04d0c31bb3199b9ef536bfa0ca086801e3
Franz Gutsch <franz.balthasar-git@gmail.com>
2018-01-03 11:07:09 +0100

    Progress in ramp trimming.

--- pyodine/analysis/signals.py
+++ pyodine/analysis/signals.py
@@ -1,6 +1,41 @@
 """Extract information from various spectroscopy signals."""
 
 from typing import Optional
+import numpy as np
+
+from .. import constants as cs
 
 def locate_doppler_line() -> Optional[float]:
     pass
+
+def trim_daq_scan(data: np.ndarray) -> np.ndarray:
+    """Extract and return the reliable part of a full DAQ scan.
+
+    As the DAQ uses a Z-shaped signal for ramp scanning, there is mostly
+    useless data at the beginning and end of the scan.  This data is trimmed
+    off.
+
+    :raises ValueError: Data has an unexpected number of columns.  We expect
+                three or two columns (ramp amplitude, error signal, <log
+                port>).
+    :returns: Array in which all columns are equally stripped from the useless
+                values at the start and end.
+    """
+    try:  # Extract columns.
+        ramp, error, log = data
+    except ValueError:
+        ramp, error = data
+
+    # Due to the nature of the hack around the DAQ asynchronicity, time (read:
+    # sample index) is an unsafe base to rely calculations on.  We use the
+    # amplitude of the loopback-ed ramp instead.
+    shave_marks = (cs.DAQ_LOG_RAMP_TRIM_FACTORS if log
+                   else cs.DAQ_ERR_RAMP_TRIM_FACTORS)
+    ramp_max, ramp_min = ramp.max(), ramp.min()
+    span = ramp_max - ramp_min
+    # Calculate ramp values between which we expect reliable data.
+    upper = ramp_max - (shave_marks[0] * span)
+    lower = ramp_min + (shave_marks[1] * span)
+    # Find the indices at which those values are.  This is a little tricky, as
+    # due to the Z shape of the ramp, both values are passed twice.
+
--- pyodine/constants.py
+++ pyodine/constants.py
@@ -14,6 +14,22 @@ As for the physical capabilities of the DAQ device, this must not exceed 20
 volts.
 """
 
+DAQ_ERR_RAMP_TRIM_FACTORS = [0.1, 0.05]  # FIXME Use real trim factors here.
+"""Trim these percentages (begin, end) off a ramp scan for err. sig. readings.
+
+As the DAQ uses a Z-shaped signal for ramp scanning, there is mostly
+useless and potentially misleading data at the beginning and end of the scan.
+"""
+
+DAQ_LOG_RAMP_TRIM_FACTORS = [0.2, 0.05]  # FIXME Use real trim factors from notes here.
+"""Trim these percentages (begin, end) off a ramp scan for log port readings.
+
+As the DAQ uses a Z-shaped signal for ramp scanning, there is mostly
+useless and potentially misleading data at the beginning and end of the scan.
+This is especially true for the logarithmic output port of the spectroscopy
+module, as it has some low-pass behaviour.
+"""
+
 DAQ_SCAN_TIME = 0.5
 """The time to take for a frequency scan in seconds."""
 
