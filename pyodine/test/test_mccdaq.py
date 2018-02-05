320172b0fea779b44b17705fdcaaadd10bb9c264
Franz Gutsch <franz.balthasar-git@gmail.com>
2018-02-05 13:39:45 +0100

    test_mccdaq.py was a dummy.

--- pyodine/test/test_mccdaq.py
+++
@@ -1,9 +0,0 @@
-"""Unit tests for the MCC DAQ driver.
-
-This module is not to be run manually but will instead be found and invoked
-automatically by the Pytest test suite.
-"""
-import pytest
-from pyodine.drivers.mccdaq import MccDaq
-
-__author__ = 'Franz Gutsch'
