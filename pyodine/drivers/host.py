88d2a44d61bf523dd93218385c5052b5a3efa477
Franz Gutsch <franz.balthasar+github@gmail.com>
2018-04-27 16:48:23 +0100

    Some basic logging in host.py.

--- pyodine/drivers/host.py
+++ pyodine/drivers/host.py
@@ -1,7 +1,11 @@
 """Controls aspects of the host computer that is running pyodine."""
 
+import logging
 import os
+
+LOGGER = logging.getLogger("host")
 
 def reboot() -> None:
     """Do a system reboot. Does not gracefully stop anything."""
+    LOGGER.warning("Rebooting host computer now...")
     os.system('reboot')
